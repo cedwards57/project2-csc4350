6a476fae809a66b17a49914b519b8a47425b4979
cedwards57 <cedwards57@student.gsu.edu>
2021-11-04 15:21:59 -0400

    database functions written and added

--- models.py
+++ models.py
@@ -4,33 +4,36 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class UserLogin(db.Model, UserMixin):
-    username = db.Column(db.String(30), primary_key="True")
+class User(db.Model, UserMixin):
+    email = db.Column(db.String(30), primary_key="True")
+    name = db.Column(db.String(30))
 
     def __repr__(self):
-        return "<UserLogin %r %r>" % (self.username)
+        return "<User %r %r>" % (self.email)
 
     def get_id(self):
-        return self.username
+        return self.email
 
 
 class SavedRecipe(db.Model):
-    username = db.Column(db.String(30), primary_key="True")
+    email = db.Column(db.String(30), primary_key="True")
     recipe_id = db.Column(db.String(80), primary_key="True")
 
     def __repr__(self):
-        return "<SavedRecipe %r %r>" % (self.username, self.recipe_id)
+        return "<SavedRecipe %r %r>" % (self.email, self.recipe_id)
 
     def get_artist_id(self):
         return self.recipe_id
 
 
 class SavedIngredient(db.Model):
-    username = db.Column(db.String(30), primary_key="True")
+    email = db.Column(db.String(30), primary_key="True")
     ingredient_id = db.Column(db.String(80), primary_key="True")
+    quantity = db.Column(db.Integer)
+    units = db.Column(db.String(80))
 
     def __repr__(self):
-        return "<SavedIngredient %r %r>" % (self.username, self.ingredient_id)
+        return "<SavedIngredient %r %r>" % (self.email, self.ingredient_id)
 
     def get_artist_id(self):
         return self.ingredient_id
