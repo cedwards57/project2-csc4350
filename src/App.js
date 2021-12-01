4d8c35410932da16690270a0c9faaf07344ad2b4
LanlanWU <77217707+LanlanWU@users.noreply.github.com>
2021-11-30 22:27:26 -0500

    cssrename

--- src/App.js
+++ src/App.js
@@ -67,8 +67,14 @@ function App() {
 
   return (
     <>
+    <ul>
+      <li><a href="/recipelist">Recipe List</a></li>
+      <li><a href="/">Grocery List</a></li>
+      <li><a href="/logout">logout</a></li>
+    </ul>
+    
     <h1>{args.name}</h1>
-    <p>This page is a work in progress. The search function may take some time to complete.</p>
+    <h2>This page is a work in progress. The search function may take some time to complete.</h2>
     <input type="text" id="searchQuery" placeholder="Search by ingredient..." /><button onClick={searchRecipe}>Search</button>
     {searchRecipes.map((item, k) => <p><ResultRecipe id={item.id} title={item.title}/><button onClick={() => addRecipe(item)}>Add Recipe</button></p>)}
     <h3>Your Recipes</h3>
@@ -77,5 +83,6 @@ function App() {
     </>
   );
 }
+
 
 export default App;
